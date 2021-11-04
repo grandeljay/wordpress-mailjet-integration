a073cafaebe3d85a68bcb4fc57edeec94815f4cf
Jay <github.hybridsupply@grandel.anonaddy.me>
2021-11-04 10:31:27 +0100

    Improve config

--- inc/config-example.php
+++ inc/config-example.php
@@ -1,8 +1,21 @@
 <?php
 
 /**
+ * config-example.php
+ *
  * @author Jay Trees <j.trees@hybridsupply.de>
  */
 
+/**
+ * The Mailjet API keys.
+ */
 define( 'MJ4WP_API_KEY', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' );
 define( 'MJ4WP_API_SECRET', 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' );
+
+/**
+ * The Mailjet template ID to use to confirm user emails.
+ */
+define( 'MJ4WP_TEMPLATE_CONFIRMATION', 0 );
+
+define( 'MJ4WP_TEMPLATE_FROM_EMAIL', 'email@domain.tld' );
+define( 'MJ4WP_TEMPLATE_FROM_NAME', 'Company Name' );
--- mailjet-for-wordpress.php
+++ mailjet-for-wordpress.php
@@ -17,39 +17,3 @@
 
 require 'vendor/autoload.php';
 require 'inc/config.php';
-
-use MailjetResources;
-
-$mj = new MailjetClient(getenv('MJ_APIKEY_PUBLIC'), getenv('MJ_APIKEY_PRIVATE'),true,['version' => 'v3.1']);
-$body = [
-    'Messages' => [
-    [
-        'From' => [
-        'Email' => "info@hybridsupply.uk",
-        'Name' => "HybridSupply"
-        ],
-        'To' => [
-        [
-            'Email' => "passenger1@example.com",
-            'Name' => "passenger 1"
-        ]
-        ],
-        'TemplateID' => 3311910,
-        'TemplateLanguage' => true,
-        'Subject' => "Confirm your email",
-        'Variables' => json_decode('{}', true)
-    ]
-    ]
-];
-$response = $mj->post(Resources::$Email, ['body' => $body]);
-$response->success() && var_dump($response->getData());
-
-if ( $response->success() ) {
-	echo '<pre>';
-	var_dump( $response->getData() );
-	echo '</pre>';
-} else {
-	echo '<pre>';
-	var_dump( $response );
-	echo '</pre>';
-}
