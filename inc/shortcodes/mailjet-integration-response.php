0c22d480395d84cda6475a73f69412afa046b5f9
Jay Trees <github.hybridsupply@grandel.anonaddy.me>
2021-12-09 09:46:02 +0100

    Sanitise input

--- inc/shortcodes/mailjet-integration-response.php
+++ inc/shortcodes/mailjet-integration-response.php
@@ -18,11 +18,11 @@ function gjmji_shortcode_mailjet_integration_response( $atts = array(), $content
 
 	// phpcs:disable WordPress.Security.NonceVerification.Recommended
 	if ( isset( $_GET['gjmji-response'] ) ) {
-		$response .= $_GET['gjmji-response'];
+		$response .= sanitize_text_field( wp_unslash( $_GET['gjmji-response'] ) );
 	}
-	if ( isset( $_GET['ErrorMessage'] ) ) {
-		$response .= '<strong>' . $_GET['StatusCode'] . '</strong> - ';
-		$response .= stripslashes( $_GET['ErrorMessage'] );
+	if ( isset( $_GET['StatusCode'], $_GET['ErrorMessage'] ) ) {
+		$response .= '<strong>' . sanitize_text_field( wp_unslash( $_GET['StatusCode'] ) ) . '</strong> - ';
+		$response .= sanitize_text_field( wp_unslash( $_GET['ErrorMessage'] ) );
 	}
 	// phpcs:enable WordPress.Security.NonceVerification.Recommended
 
